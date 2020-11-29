8b2d935508fedec5d75082720bf0afa22d495a0d
behramshah <behramshax@gmail.com>
2020-11-29 16:59:55 +0400

    natamam

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+const addButton = document.getElementById("Add");
+
+function createtask () {
+    const task=document.createElement("li");
+    const textnode = document.createTextNode("Task");
+    node.appendChild(textnode);
+    document.getElementsByTagName("ul").appendChild(node);
+}
+
+addButton.addEventListener("click", createtask());
+    
