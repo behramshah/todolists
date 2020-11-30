6a933cf8897a8e3395534458979c4990ff3a9836
Bahram <37588033+behramshah@users.noreply.github.com>
2020-11-30 09:01:37 +0400

    update js

--- index.js
+++ index.js
@@ -2,13 +2,39 @@
 
 
 const addButton = document.getElementById("Add");
+const deleteButton = document.getElementById("delete")
+const doneButton = document.getElementById("done")
+
 
 function createtask () {
-    const task=document.createElement("li");
-    const textnode = document.createTextNode("Task");
-    node.appendChild(textnode);
-    document.getElementsByTagName("ul").appendChild(node);
+    const inputValue = document.getElementById("input").value;
+    if(inputValue == "")
+    return;
+    const task = document.createElement("li");
+    task.classList = "line"
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.id ="delete";
+    const doneButton = document.createElement("button");
+    doneButton.innerText = "Done";
+    doneButton.id ="done"
+    task.innerHTML = inputValue;
+    document.getElementById("List").appendChild(task);
+    task.appendChild(doneButton);
+    task.appendChild(deleteButton);
+    document.getElementById("input").value = "";
+        return;    
 }
-
-addButton.addEventListener("click", createtask());
+function deleteTask(){
+    const line = this.parentNode
+    line.remove();
+    return;    
+};
+function taskDone(){
+    
+}
+    
+addButton.addEventListener("click", createtask);
+deleteButton.addEventListener("click",deleteTask);
+doneButton.addEventListener("click",taskDone);
     
